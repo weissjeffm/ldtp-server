22cfce11c3db27ac8b8d786b1e619adb7f3e2e60
Jeff Weiss <jweiss@redhat.com>
2010-09-27 10:48:57 -0400

    developing java client for ldtp

---
+++ client/java/src/com/redhat/qe/ldtpclient/Generator.java
@@ -0,0 +1,109 @@
+package com.redhat.qe.ldtpclient;
+
+import java.io.StringWriter;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
+import org.apache.velocity.exception.MethodInvocationException;
+import org.apache.velocity.exception.ParseErrorException;
+import org.apache.velocity.exception.ResourceNotFoundException;
+
+/**
+ * @author jweiss
+ *
+ *	Generates an xmlrpc client for python LDTP xmlrpc server.  This allows you
+ *	to remotely control GTK (and java and other UIs) apps.  If the LDTP server
+ *  API changes, you can make changes to the the API list here and regenerate
+ *  the client.
+ */
+public class Generator {
+
+	public class APIMethod {
+		public String name;
+		public Class<?> returnType;
+		public APIParameter[] parameters;
+		public APIMethod(String name, Class<?> returnType) {
+			super();
+			this.name = name;
+			this.returnType = returnType;
+		}
+		public APIMethod(String name, Class<?> returnType, String[] paramNames, Class<?>... paramTypes){
+			this(name, returnType);
+			this.parameters = new APIParameter[paramNames.length];
+			for (int i=0; i<paramNames.length; i++){
+				this.parameters[i] = new APIParameter(paramNames[i], paramTypes[i]);
+			}
+		}
+
+		public APIMethod(String name, Class<?> returnType, String param, Class<?> paramType){
+			this(name, returnType);
+			this.parameters = new APIParameter[1];
+			parameters[0] = new APIParameter(param, paramType);
+		}
+
+		public APIMethod(String name, Class<?> returnType, String param, Class<?> paramType, String param2, Class<?> param2Type){
+			this(name, returnType);
+			this.parameters = new APIParameter[2];
+			parameters[0] = new APIParameter(param, paramType);
+			parameters[1] = new APIParameter(param2, param2Type);
+		}
+		public APIMethod(String name, Class<?> returnType, String param, Class<?> paramType, String param2, Class<?> param2Type, String param3, Class<?> param3Type){
+			this(name, returnType);
+			this.parameters = new APIParameter[3];
+			parameters[0] = new APIParameter(param, paramType);
+			parameters[1] = new APIParameter(param2, param2Type);
+			parameters[1] = new APIParameter(param3, param3Type);
+		}
+
+	}
+	
+	public class APIParameter {
+		public String name;
+		public Class<?> type;
+		public APIParameter(String name, Class<?> type) {
+			super();
+			this.name = name;
+			this.type = type;
+		}
+	}
+	public static void main(String... args) throws Exception{
+		Generator g = new Generator();
+		Set<Generator.APIMethod> api = new HashSet<Generator.APIMethod>();
+		api.add(g.new APIMethod("launchapp", Integer.class, "args", String[].class));
+		api.add(g.new APIMethod("click", Integer.class, "windowName", String.class, "componentName", String.class));
+		
+		Velocity.init();
+		VelocityContext context = new VelocityContext();
+	
+		context.put( "name", new String("Velocity") );
+	
+		Template template = null;
+	
+		try
+		{
+		   template = Velocity.getTemplate("mytemplate.vm");
+		}
+		catch( ResourceNotFoundException rnfe )
+		{
+		   // couldn't find the template
+		}
+		catch( ParseErrorException pee )
+		{
+		  // syntax error: problem parsing the template
+		}
+		catch( MethodInvocationException mie )
+		{
+		  // something invoked in the template
+		  // threw an exception
+		}
+		catch( Exception e )
+		{}
+	
+		StringWriter sw = new StringWriter();
+	
+		template.merge( context, sw );
+	}
+}
--- client/java/src/com/redhat/qe/ldtpclient/LDTPClient.java
+++ client/java/src/com/redhat/qe/ldtpclient/LDTPClient.java
@@ -19,7 +19,57 @@ public class LDTPClient {
 	}
 	
 	public Integer launchApp(String binary, String[] arguments) throws Exception {
-		return (Integer)session.getClient().execute("launchapp", new Object[] {binary, arguments});
+		return invoke("launchapp", Integer.class, binary, arguments);
+	}
+
+	public Integer click(String windowName, String componentName) throws Exception {
+		return invoke("click", Integer.class, windowName, componentName);
+	}
+
+	public Integer stateEnabled(String windowName, String componentName) throws Exception {
+		return invoke("stateenabled", Integer.class, windowName, componentName);
+	}
+	
+	public Integer check(String windowName, String componentName) throws Exception {
+		return invoke("check", Integer.class, windowName, componentName);
+	}
+	
+	public Integer uncheck(String windowName, String componentName) throws Exception {
+		return invoke("uncheck", Integer.class, windowName, componentName);
+	}
+
+	public Integer verifyCheck(String windowName, String componentName) throws Exception {
+		return invoke("verifycheck", Integer.class, windowName, componentName);
+	}
+
+	public Integer getTableRowIndex(String windowName, String componentName, String tableName, String cellValue) throws Exception {
+		return invoke("gettablerowindex", Integer.class, windowName, componentName, tableName, cellValue);
+	}
+	
+	public Integer checkRow(String windowName, String componentName, Integer rowIndex, Integer colIndex) throws Exception {
+		return invoke("checkrow", Integer.class, windowName, componentName, rowIndex, colIndex);
+	}
+	
+	public String[] getAllStates(String windowName, String componentName) throws Exception {
+		return invoke("getallstates", String[].class, windowName, componentName);
+	}
+
+	public Integer hasstate(String windowName, String componentName, String state) throws Exception {
+		return invoke("hasstate", Integer.class, windowName, componentName, state);
+	}
+
+	public Integer selectRowPartialMatch(String windowName, String componentName, String matchText) throws Exception {
+		return invoke("selectrowpartialmatch", Integer.class, windowName, componentName, matchText);
+	}
+
+	public Integer waitTilGuiExist(String windowName, String componentName) throws Exception {
+		return invoke("waittillguiexist", Integer.class, windowName, componentName);
+	}
+
+
+
+	public <T>T invoke(String methodName, Class<T> returnType, Object... args) throws Exception{
+		return (T)session.getClient().execute(methodName, args);
 	}
 	public static void main(String... args) throws Exception{
 		LDTPClient client  = new LDTPClient("http://jweiss-rhel6-1.usersys.redhat.com:8001/");
