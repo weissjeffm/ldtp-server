5b29d400a506f54c2985817d7547f5ffc515ca16
Jeff Weiss <jweiss@redhat.com>
2010-12-02 10:30:19 -0500

    now extract the number of optional arguments as well

--- extract-api.py
+++ extract-api.py
@@ -6,6 +6,13 @@ from ldtpd.xmlrpc_daemon import XMLRPCLdtpd
 xmlrpc = XMLRPCLdtpd()
 public_apis = filter(lambda fn: fn.startswith("xmlrpc_"),  dir(xmlrpc))
 names = map(lambda n: n.split("xmlrpc_")[1], public_apis) #strip prefix off fn names
-argz  = map(lambda f: inspect.getargspec(getattr(xmlrpc,f)).args[1:], public_apis)
+specs = map(lambda n:  inspect.getargspec(getattr(xmlrpc,n)), names)
+def defaultCount(spec):
+  x = spec.defaults
+  if (x): 
+    return len(x)
+  else:
+    return 0
+argz  = map(lambda s: [ s.args[1:], defaultCount(s)], specs)
 api = zip(names, argz)
-print(api)
+print(str(api).replace("'", '"'))
